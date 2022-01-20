87e227e364b8b661ff5bbb9cc0b13c1b113b4505
parsa011 <parsa32569@gmail.com>
2022-01-20 13:11:00 +0330

    change exercise 1.1 lexer and ...

--- compiler/exercises/1.1/main.c
+++ compiler/exercises/1.1/main.c
@@ -1,19 +1,134 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include "def.h"
+#include <string.h>
+#include <ctype.h>
 
-#define PROMPT ">>"
+char *buf;
+int length = 0;
 
-void write_prompt();
+/* current token (no struct :) )*/
+char token[32];
+int type;
 
-int main()
+int getl(char *);
+int readl();
+
+/* also we can use enums */
+#define EOI			0
+#define SEMI		1
+#define	PLUS		2
+#define	TIMES		3
+#define	LP			4
+#define RP			5
+#define	NUM_OR_ID	6
+const char *token_names[]  = {
+	"End of Input",
+	"Semicolon",
+	"Plus",
+	"Times",
+	"Left Parenthesis",
+	"Right Parenthesis",
+	"Number Or Id"
+};
+
+/* lexer */
+int lookahead = -1;
+int match(int);
+void advance();
+int next();
+
+int main(int argc,char *argv[])
 {
-	statements();
+	buf = (char *)malloc(20);
+
+	do {
+		readl();
+		type = next();
+		printf("current token is %d and text is : %s\n",type,token);
+	} while (!match(EOI));
+
 	return 0;
 }
 
-void write_prompt()
+int readl()
 {
-	printf("%s ",PROMPT);
+	printf(">>> ");
+	length = getl(buf);
+}
+
+int getl(char *line)
+{
+	int i = 0;
+	int c;
+	while ((c = getchar()) != EOF) {
+		if (c == '\n')
+			break;
+		line[i++] = c;
+	}
+	line[i] = 0;
+	return i;
+}
+
+int next()
+{
+	static int offset = 0;
+	if (offset == length) {
+		offset = 0;
+		token[offset] = 0;
+		return EOI;
+	}
+	char *p = buf + offset;
+	char *tokenp = token;
+	while (*p) {
+			*tokenp++ = *p;
+			*tokenp++ = 0;
+			length = 1;
+			switch (*p) {
+				case EOF : 
+					return EOI;
+				case 'i' : 
+					return SEMI;
+				case '+' : 
+					return PLUS;
+				case '*' : 
+					return TIMES;
+				case '(' : 
+					return LP;
+				case ')' : 
+					return RP;
+				case ';' :
+					return SEMI;
+				case '\n' :
+				case '\t' :
+				case ' '  :
+					break;
+				default :
+					if (!isalnum(*p))
+						fprintf(stderr,"Ignoring illegal input <%c>\n", *p);
+					else {
+						tokenp--;
+						while (isalnum(*p)) {
+							p++;
+							*tokenp++ = *p;
+						}
+						length = p - buf;
+						*tokenp++ = 0;
+						return NUM_OR_ID;
+					}
+					break;			
+		}
+		p++;
+	}
+}
+
+int match(int token)
+{
+	if (lookahead == -1)
+		lookahead = next();
+	return lookahead == token;
+}
+
+void advance()
+{
+	lookahead = next();
 }
