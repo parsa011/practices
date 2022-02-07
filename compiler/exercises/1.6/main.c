51469f5dde92a51458d596a87f08cc7091ac3844
parsa mahmoudy <parsa32569@gmail.com>
2022-02-07 08:43:33 +0330

    fix lexer problem

--- compiler/exercises/1.6/main.c
+++ compiler/exercises/1.6/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef struct lexer_t {
-	  FILE *file;
+    FILE *file;
     int lineno;
     int offset;
     char *buf;  // current line
@@ -10,7 +13,7 @@ typedef struct lexer_t {
 } lexer;
 
 typedef struct token_t {
-	char value[32];
+    char value[32];
     int val;
     int type;
     int lineno;
@@ -18,7 +21,7 @@ typedef struct token_t {
 } token;
 
 enum {
-	OPEN_PARENTHESIS,
+    OPEN_PARENTHESIS = 1,
     CLOSE_PARENTHESIS,
     PLUS,
     MINUS,
@@ -26,16 +29,104 @@ enum {
     DIV,
     NUMBER,
     POW,
-    BAD
+    WHITESPACE,
+    BAD,
+    EOI
 };
 
 void die(char *);
 void open_file(char *);
 int readl();
 
+void reset_token();
+
+// lexer stuff
+void next();
+bool match();
+
 // current lexer and token
 lexer *lex;
 token *tok;
+
+void next()
+{
+  reset_token();
+  if (!lex->buf || lex->offset >= lex->length - 1) {
+    lex->offset = 0;
+    if (readl() == EOF) {
+      tok->type = EOI;
+      return;
+    } 
+  }
+  char *p = lex->buf + lex->offset;
+  if (*p) {
+    tok->len = 1;
+    tok->lineno = lex->lineno;
+
+    while (isspace(*p)) {
+      p++;
+      lex->offset++;
+    }
+
+    tok->value[0] = *p;
+    switch (*p) {
+      case '(' :
+        tok->type = OPEN_PARENTHESIS;
+        break;
+      case ')' :
+        tok->type = CLOSE_PARENTHESIS;
+        break;
+      case '+' :
+        tok->type = PLUS;
+        break;
+      case '-' :
+        tok->type = MINUS;
+        break;
+      case '/' :
+        tok->type = DIV;
+        break;
+      case '*' :
+        tok->type = STAR;
+        break;
+      case '^' :
+        tok->type = POW;
+        break;
+      case '\n':
+      case '\t':
+        tok->type = WHITESPACE;
+        break;
+      default :
+        if (isdigit(*p)) {
+          char temp[10];
+          int index = 1;
+          temp[0] = *p++;
+          while (isdigit(*p)) {
+            temp[++index] = *p++;
+            lex->offset++;
+          }
+          strcpy(tok->value,temp);
+          tok->type = NUMBER;
+          tok->val = atoi(temp);
+          tok->len = strlen(temp);
+        } else {
+          tok->type = BAD;
+        }
+        break;
+    }
+  }
+  lex->offset++;
+}
+
+bool match(int type)
+{
+  return tok->type == type;
+}
+
+void reset_token()
+{
+  tok->lineno = tok->len = tok->val = tok->type = 0;
+  tok->value[0] = '\0';
+}
 
 void die(char *msg)
 {
@@ -43,8 +134,10 @@ void die(char *msg)
   exit(0);
 }
 
+// actully this is a little startup for program XDD
 void open_file(char *name)
 {
+  tok = malloc(sizeof(token));
   lex = malloc(sizeof(lexer));
   lex->file = fopen(name,"r");
   if (!lex->file) {
@@ -56,7 +149,10 @@ int readl()
 {
   if (!lex)
     die("first run a lexer");
-  return getline(&lex->buf,&lex->length,lex->file) == EOF;
+  lex->lineno++;
+  int read = getline(&lex->buf,&lex->length,lex->file);
+  lex->length = strlen(lex->buf);
+  return read;
 }
 
 int main(int argc,char *argv[])
@@ -65,5 +161,9 @@ int main(int argc,char *argv[])
     die("enter file name");
   }
   open_file(argv[1]);
+  do {
+    next();
+    printf("val is %s\n",tok->value);
+  } while (!match(EOI));
   return 0;
 }
