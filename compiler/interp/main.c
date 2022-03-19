2110d550d12e5f28508e6fc4e1827b116da5361c
parsa011 <parsa32569@gmail.com>
2022-03-19 10:28:11 +0330

    working on parser and print ast and ...

--- compiler/interp/main.c
+++ compiler/interp/main.c
@@ -14,11 +14,11 @@
 void panic(char *, ...);
 #define CHR_T_INT(c) (c - '0')
 
-// file
-void open_file(char *);
+// fp
+void open_fp(char *);
 char read_char();
-void close_file();
-bool file_exist(char *);
+void close_fp();
+bool fp_exist(char *);
 
 // lexer
 // we have to pass lexer to each function , but this is simple program and we dont need 
@@ -37,7 +37,17 @@ struct ASTnode *mkastleaf(int, int);
 struct ASTnode *mkastunary(int, struct ASTnode *, int);
 
 //parser
+void match(int, char *);
+void semi();
+void statements();
+void print_statement();
+void variable_decleration_statement();
+void assigin_statement();
 
+void print_ast(struct ASTnode *, int);
+
+struct ASTnode *binary_expression(int);
+int calculate_binary_tree(struct ASTnode *);
 
 // declerations
 
@@ -104,12 +114,32 @@ struct ASTnode {
 	int value;
 };
 
+enum ArithOp {
+	A_ADD,
+	A_SUB,
+	A_MULTIPLY,
+	A_DIVIDE,
+	A_POW,
+	A_CONST,
+	A_IDENT
+};
+
+char *ArithOp_str[] = {
+	"A_ADD",
+	"A_SUB",
+	"A_MULTIPLY",
+	"A_DIVIDE",
+	"A_POW",
+	"A_CONST",
+	"A_IDENT"
+};
+
 char Text[32];
 int Text_len;
 
 //var
 struct Variable *create_var(char *, int);
-struct Variable *find_var(int);
+int find_var(char *);
 
 #define VARIABLE_MAX_LENGTH 32
 // now we just have int variables :)))))))))
@@ -145,9 +175,9 @@ bool interp_mode = false;
 #define c_token (lexer.token)
 #define set_c_token_type(t) (c_token.type = t)
 
-// file
+// fp
 
-bool file_exist(char *f)
+bool fp_exist(char *f)
 {
 	return access(f, F_OK) == 0;
 }
@@ -157,9 +187,9 @@ char read_char()
 	return fgetc(lexer.fp);
 }
 
-void open_file(char *f)
+void open_fp(char *f)
 {
-	if (!file_exist(f)) {
+	if (!fp_exist(f)) {
 		panic("File %s, Dosnt Exist", f);
 	}
 	lexer.fp = fopen(f, "r");
@@ -171,7 +201,7 @@ void set_program_mode(int argc, char *argv[])
 		interp_mode = true;
 		lexer.fp = stdin;
 	} else {
-		open_file(argv[1]);
+		open_fp(argv[1]);
 	} 
 }
 
@@ -217,6 +247,7 @@ int scan_const(int c)
 	while (isdigit((c = next_char()))) {
 		res = res * 10 + CHR_T_INT(c);
 	}
+	return res;
 }
 
 void scan_ident(int c)
@@ -310,15 +341,206 @@ struct ASTnode *mkastunary(int op, struct ASTnode *left, int value)
 	return mkastnode(op, left, NULL, value);
 }
 
+// variables
+int find_var(char *name)
+{
+	int index;
+	for (int i = 0; i < variables_count; i++) {
+		if (strcmp(name, variables[i]->name) == 0)
+			return index;
+	}
+	return -1;
+}
+
+// parser
+void match(int type, char *msg)
+{
+	if (c_token.type != type)
+		panic("Error In Line %d : %s", lexer.lineno, msg);
+	lex();
+}
+
+void semi()
+{
+	match(T_SEMI, "Mised Semicolon");
+}
+
+void statements()
+{
+	while (c_token.type != T_EOI) {
+		switch (c_token.type) {
+			case T_INT :
+				variable_decleration_statement();
+				break;
+			case T_IDENT :
+				assigin_statement();
+				break;
+			case T_PRINT :
+				print_statement();
+				break;
+			default :
+				panic("Error On line %d : Bad Token\n", lexer.lineno);
+		}
+	}
+}
+
+void eat_open_parenthesis()
+{
+	match(T_OP, "Missed Open Parenthesis");
+}
+
+void eat_close_parenthesis()
+{
+	match(T_CP, "Missed Close Parenthesis");
+}
+
+void print_statement()
+{
+	match(T_PRINT, "Print Expected");
+	eat_open_parenthesis();
+	struct ASTnode *n = binary_expression(0);
+	print_ast(n, 0);
+	//eat_close_parenthesis();
+	semi();
+	int result = calculate_binary_tree(n);
+	printf("%d\n", result);
+}
+
+void assigin_statement()
+{
+
+}
+
+void variable_decleration_statement()
+{
+
+}
+
+int arithop(int type)
+{
+	switch (type) {
+		case T_PLUS :
+			return A_ADD;
+		case T_DASH :
+			return A_SUB;
+		case T_SLASH :
+			return A_DIVIDE;
+		case T_STAR :
+			return A_MULTIPLY;
+		case T_POW :
+			return A_POW;
+		case T_CONST :
+			return A_CONST;
+	}
+	panic("Invalid Arithmetic Operation %s", Tokens_str[type]);
+}
+
+const int OpPrec[] = {
+	10, 10, 20, 20, 20,
+
+	0, 0,
+
+	0, 0, 10, 10, 0,
+
+	0, 0
+};
+
+struct ASTnode *primary()
+{
+	struct ASTnode *n;
+	int index;
+	switch (c_token.type) {
+		case T_CONST :
+			n = mkastleaf(A_CONST, c_token.value);
+			lex();
+			return n;
+		case T_IDENT :
+			index = find_var(Text);
+			if (index == -1)
+				panic("Variable '%s' not found", Text);
+			n = mkastleaf(A_IDENT, index);
+			lex();
+			return n;
+	}
+	return n;
+}
+
+int op_precedence(int tokentype) {
+	int prec = OpPrec[tokentype];
+	if (prec == 0)
+		panic("Syntax error, token %d", tokentype);
+	return (prec);
+}
+
+struct ASTnode *binary_expression(int ptp)
+{
+	struct ASTnode *left, *right;
+	int tokentype;
+
+	left = primary();
+	tokentype = c_token.type;
+	if (tokentype == T_SEMI || tokentype == T_CP)
+		return left;
+
+	while (op_precedence(tokentype) > ptp) {
+		lex();
+		right = binary_expression(OpPrec[tokentype]);
+		left = mkastnode(arithop(tokentype), left, right, 0);
+		tokentype = c_token.type;
+		if (tokentype == T_SEMI || tokentype == T_CP)
+			break;	
+	}
+	return left;
+}
+
+int calculate_binary_tree(struct ASTnode *n)
+{
+	int left, right;
+	if (n->left)
+		left = calculate_binary_tree(n->left);
+	if (n->right)
+		right = calculate_binary_tree(n->right);
+	switch (n->op) {
+		case A_ADD :
+			return left + right;
+		case A_SUB :
+			return left - right;
+		case A_MULTIPLY :
+			return left * right;
+		case A_DIVIDE :
+			return left / right;
+		case A_CONST :
+			return n->value;
+		case A_IDENT :
+			return variables[n->value]->value;
+	}
+}
+
+void print_ast(struct ASTnode *n, int depth)
+{
+	for (int i = 1; i <= depth; i++) {
+		if (depth % i == 0 && i != depth && i != 2)
+			putchar('|');
+		else
+			putchar(' ');
+	}
+	printf("%s\n", ArithOp_str[n->op]);
+	if (n->left) {
+		print_ast(n->left, depth + 4);
+	}
+	if (n->right) {
+		print_ast(n->right, depth + 4);
+	}
+}
+
 // main
 
 int main(int argc, char **argv)
 {
 	init_variables_table();
 	set_program_mode(argc, argv);
-	while (lex()) {
-		printf("%s\n", Tokens_str[c_token.type]);
-	}
+	lex();
+	statements();
 	return 0;
 }
 
